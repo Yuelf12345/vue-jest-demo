7da674e11fff0193b05c781e9fa11ec4e14a7846
Yuelf12345 <2794127100@qq.com>
2025-06-13 17:58:08 +0800

    chore:项目初始化

---
+++ src/tests/unit/HelloWorld.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+describe('HelloWorld.vue', () => {
+  it('renders properly', () => {
+    const wrapper = mount(HelloWorld);
+    expect(wrapper.exists()).toBe(true);
+  });
+});

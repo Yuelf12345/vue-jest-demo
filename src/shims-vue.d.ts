76d64c31e65291a389d6facf0de8aacc90029eb9
Yuelf12345 <2794127100@qq.com>
2025-06-18 10:50:18 +0800

    chore: tsx 适配

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,10 +1,4 @@
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
-declare module '*.tsx' {
+declare module '*' {
   import { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
